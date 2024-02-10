e988b675f457b30be0d9e909e37341d8e9f0611f
Myk Escala <rapidstrike13@gmail.com>
2024-02-10 03:14:06 +0000

    headphones page

---
+++ src/app/headphones/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/footer"
+import Header from "@/components/header"
+
+export default function Headphones(){
+    return(
+        <>
+            <main>
+                <div className="pt-[195px] pb-[95px] bg-black">
+                 <Header />
+                </div>
+            </main>
+            <Footer/>
+        </>
+    )
+}
