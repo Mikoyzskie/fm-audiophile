5c7f4ac78e08e3ad16044063d9958258a11a5818
Mikoyzskie <rapidstrike13@gmail.com>
2024-02-11 21:22:56 +0800

    proper merge

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,17 +7,7 @@ export default function Header() {
     const navs = ["home", "headphones", "speakers", "earphones"]
 
     return (
-<<<<<<< HEAD
-        <header id='home' className='max-w-[1110px] w-full mx-auto pt-8 pb-9 border-b border-[rgba(255,255,255,0.2)] flex justify-between items-center'>
-            <Link href={"#"}>
-                <Image
-                    src={"/shared/desktop/logo.svg"}
-                    alt='logo'
-                    width={143}
-                    height={25}
-                />
-            </Link>
-=======
+
         <header className='max-w-[1110px] w-full mx-auto pt-8 pb-9 border-b border-[rgba(255,255,255,0.2)] flex justify-between items-center '>
             <Image
                 src={"/shared/desktop/logo.svg"}
@@ -25,7 +15,7 @@ export default function Header() {
                 width={143}
                 height={25}
             />
->>>>>>> e988b675f457b30be0d9e909e37341d8e9f0611f
+
             <ul className='flex gap-[34px]'>
                 {
                     navs.map((nav: string) => (
